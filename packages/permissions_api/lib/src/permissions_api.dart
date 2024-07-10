91cb11e954b7947a203cc6c0f09995d17898602c
Grzesiek <gzaber@tlen.pl>
2024-07-10 14:28:37 +0200

    feat: add method for observing permissions by category id

--- packages/permissions_api/lib/src/permissions_api.dart
+++ packages/permissions_api/lib/src/permissions_api.dart
@@ -4,6 +4,7 @@ abstract interface class PermissionsApi {
   Future<void> createPermission(Permission permission);
   Future<void> updatePermission(Permission permission);
   Future<void> deletePermission(String id);
+  Stream<List<Permission>> observePermissionsByCategory(String categoryId);
   Stream<List<Permission>> observePermissionsByGiver(String email);
   Stream<List<Permission>> observePermissionsByReceiver(String email);
 }
