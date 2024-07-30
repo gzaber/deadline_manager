1072c732a934a1687dde7a8de81379a01416f91e
Grzesiek <gzaber@tlen.pl>
2024-07-30 21:41:10 +0200

    feat: disable integration with web browser history

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:categories_repository/categories_repository.dart';
+import 'package:deadline_manager/app/app.dart';
 import 'package:deadline_manager/firebase_options.dart';
 import 'package:deadlines_repository/deadlines_repository.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firestore_categories_api/firestore_categories_api.dart';
 import 'package:firestore_deadlines_api/firestore_deadlines_api.dart';
 import 'package:firestore_permissions_api/firestore_permissions_api.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:flutter/material.dart';
 import 'package:permissions_repository/permissions_repository.dart';
 
-import 'app/app.dart';
-
 void main() async {
+  setUrlStrategy(null);
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
