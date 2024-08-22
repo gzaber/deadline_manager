cac87a9779f8a8efd93cee40d4cceacebe76ca6a
Grzesiek <gzaber@tlen.pl>
2024-08-22 23:21:31 +0200

    test: add firestore apis implementation tests

--- packages/firestore_categories_api/test/firestore_categories_api_test.dart
+++ packages/firestore_categories_api/test/firestore_categories_api_test.dart
@@ -1,1 +1,133 @@
+import 'package:categories_api/categories_api.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firestore_categories_api/firestore_categories_api.dart';
+import 'package:flutter_test/flutter_test.dart';
 
+void main() {
+  final category1 = Category(
+    owner: 'owner',
+    name: 'name1',
+    icon: 100,
+    color: 200,
+  );
+  final category2 = Category(
+    owner: 'owner',
+    name: 'name2',
+    icon: 100,
+    color: 200,
+  );
+
+  group('FirestoreCategoriesApi', () {
+    late FirebaseFirestore fakeFirestore;
+    late FirestoreCategoriesApi firestoreCategoriesApi;
+
+    setUp(() {
+      fakeFirestore = FakeFirebaseFirestore();
+      firestoreCategoriesApi = FirestoreCategoriesApi(firestore: fakeFirestore);
+    });
+
+    group('constructor', () {
+      test('works properly', () {
+        expect(
+          () => FirestoreCategoriesApi(firestore: fakeFirestore),
+          returnsNormally,
+        );
+      });
+    });
+
+    group('createCategory', () {
+      test('inserts category into collection', () async {
+        await firestoreCategoriesApi.createCategory(category1);
+
+        final snapshot = await fakeFirestore.collection('categories').get();
+        expect(snapshot.docs.length, equals(1));
+        expect(snapshot.docs.first.data(), equals(category1.toJson()));
+      });
+    });
+
+    group('updateCategory', () {
+      test('updates existing category', () async {
+        await fakeFirestore
+            .collection('categories')
+            .doc(category1.id)
+            .set(category1.toJson());
+
+        await firestoreCategoriesApi.updateCategory(
+          category1.copyWith(name: 'new name'),
+        );
+
+        final snapshot = await fakeFirestore.collection('categories').get();
+        expect(
+          snapshot.docs.first.data(),
+          equals(category1.copyWith(name: 'new name').toJson()),
+        );
+      });
+    });
+
+    group('deleteCategory', () {
+      test('deletes existing category', () async {
+        await fakeFirestore
+            .collection('categories')
+            .doc(category1.id)
+            .set(category1.toJson());
+
+        await firestoreCategoriesApi.deleteCategory(category1.id);
+
+        final snapshot = await fakeFirestore.collection('categories').get();
+        expect(snapshot.docs.length, equals(0));
+      });
+    });
+
+    group('readCategoryById', () {
+      test('reads category by its id', () async {
+        await fakeFirestore
+            .collection('categories')
+            .doc(category1.id)
+            .set(category1.toJson());
+
+        final result =
+            await firestoreCategoriesApi.readCategoryById(category1.id);
+        expect(result, equals(category1));
+      });
+    });
+
+    group('readCategoriesByOwner', () {
+      test('reads category list by owner', () async {
+        await fakeFirestore
+            .collection('categories')
+            .doc(category1.id)
+            .set(category1.toJson());
+        await fakeFirestore
+            .collection('categories')
+            .doc(category2.id)
+            .set(category2.toJson());
+
+        final result =
+            await firestoreCategoriesApi.readCategoriesByOwner('owner');
+        expect(
+          result,
+          equals([category1, category2]),
+        );
+      });
+    });
+
+    group('observeCategoriesByOwner', () {
+      test('emits category list by owner', () async {
+        await fakeFirestore
+            .collection('categories')
+            .doc(category1.id)
+            .set(category1.toJson());
+        await fakeFirestore
+            .collection('categories')
+            .doc(category2.id)
+            .set(category2.toJson());
+
+        expect(
+          firestoreCategoriesApi.observeCategoriesByOwner('owner'),
+          emits([category1, category2]),
+        );
+      });
+    });
+  });
+}
--- packages/firestore_permissions_api/test/firestore_permissions_api_test.dart
+++ packages/firestore_permissions_api/test/firestore_permissions_api_test.dart
@@ -1,1 +1,159 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firestore_permissions_api/firestore_permissions_api.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:permissions_api/permissions_api.dart';
 
+void main() {
+  final permission1 = Permission(
+    giver: 'giver1',
+    receiver: 'receiver1',
+    categoryIds: const ['1', '2'],
+  );
+  final permission2 = Permission(
+    giver: 'giver1',
+    receiver: 'receiver2',
+    categoryIds: const ['2', '3'],
+  );
+
+  group('FirestorePermissionsApi', () {
+    late FirebaseFirestore fakeFirestore;
+    late FirestorePermissionsApi firestorePermissionsApi;
+
+    setUp(() {
+      fakeFirestore = FakeFirebaseFirestore();
+      firestorePermissionsApi =
+          FirestorePermissionsApi(firestore: fakeFirestore);
+    });
+
+    group('constructor', () {
+      test('works properly', () {
+        expect(
+          () => FirestorePermissionsApi(firestore: fakeFirestore),
+          returnsNormally,
+        );
+      });
+    });
+
+    group('createPermission', () {
+      test('inserts permission into collection', () async {
+        await firestorePermissionsApi.createPermission(permission1);
+
+        final snapshot = await fakeFirestore.collection('permissions').get();
+        expect(snapshot.docs.length, equals(1));
+        expect(snapshot.docs.first.data(), equals(permission1.toJson()));
+      });
+    });
+
+    group('updatePermission', () {
+      test('updates existing permission', () async {
+        await fakeFirestore
+            .collection('permissions')
+            .doc(permission1.id)
+            .set(permission1.toJson());
+
+        await firestorePermissionsApi.updatePermission(
+          permission1.copyWith(receiver: 'new receiver'),
+        );
+
+        final snapshot = await fakeFirestore.collection('permissions').get();
+        expect(
+          snapshot.docs.first.data(),
+          equals(permission1.copyWith(receiver: 'new receiver').toJson()),
+        );
+      });
+    });
+
+    group('deletePermission', () {
+      test('deletes existing permission', () async {
+        await fakeFirestore
+            .collection('permissions')
+            .doc(permission1.id)
+            .set(permission1.toJson());
+
+        await firestorePermissionsApi.deletePermission(permission1.id);
+
+        final snapshot = await fakeFirestore.collection('permissions').get();
+        expect(snapshot.docs.length, equals(0));
+      });
+    });
+
+    group('readPermissionsByCategoryId', () {
+      test('reads permission list by category id', () async {
+        await fakeFirestore
+            .collection('permissions')
+            .doc(permission1.id)
+            .set(permission1.toJson());
+        await fakeFirestore
+            .collection('permissions')
+            .doc(permission2.id)
+            .set(permission2.toJson());
+
+        final result =
+            await firestorePermissionsApi.readPermissionsByCategoryId('2');
+        expect(
+          result,
+          equals([permission1, permission2]),
+        );
+      });
+    });
+
+    group('readCategoryIdsByReceiver', () {
+      test('reads category ids list by receiver', () async {
+        await fakeFirestore
+            .collection('permissions')
+            .doc(permission1.id)
+            .set(permission1.toJson());
+        await fakeFirestore
+            .collection('permissions')
+            .doc(permission2.id)
+            .set(permission2.toJson());
+
+        final result = await firestorePermissionsApi
+            .readCategoryIdsByReceiver('receiver1');
+        expect(
+          result,
+          equals(['1', '2']),
+        );
+      });
+    });
+
+    group('readPermissionsByGiver', () {
+      test('reads permission list by giver', () async {
+        await fakeFirestore
+            .collection('permissions')
+            .doc(permission1.id)
+            .set(permission1.toJson());
+        await fakeFirestore
+            .collection('permissions')
+            .doc(permission2.id)
+            .set(permission2.toJson());
+
+        final result =
+            await firestorePermissionsApi.readPermissionsByGiver('giver1');
+        expect(
+          result,
+          equals([permission1, permission2]),
+        );
+      });
+    });
+
+    group('observePermissionsByGiver', () {
+      test('emits permission list by giver', () async {
+        await fakeFirestore
+            .collection('permissions')
+            .doc(permission1.id)
+            .set(permission1.toJson());
+        await fakeFirestore
+            .collection('permissions')
+            .doc(permission2.id)
+            .set(permission2.toJson());
+
+        expect(
+          firestorePermissionsApi.observePermissionsByGiver('giver1'),
+          emits([permission1, permission2]),
+        );
+      });
+    });
+  });
+}
